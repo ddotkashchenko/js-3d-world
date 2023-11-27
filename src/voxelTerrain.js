e9b66010fc5f932a4cfe60b08da37c3884b808ad
Your Name <dmytro.kashchenko@ihsmarkit.com>
2023-11-27 12:51:21 +0200

    shared vertices in middle

--- src/voxelTerrain.js
+++ src/voxelTerrain.js
@@ -16,79 +16,66 @@ export default class VoxelTerrain {
         this._cells = [];
     }
 
-    _cube(options) {
-        const { append, holes } = {
-            append: {},
-            holes: {},
-            ...options,
-        };
-
+    _construct() {
         // prettier-ignore
         const vertices = [
-            ...[-1, -1, 1], //leftBottomFront
-            ...[1, -1, 1], //rightBottomFront
-            ...[1, 1, 1], //rightTopFront
-            ...[-1, 1, 1], //leftTopFront
+            -1, -1, 1,      // 0
+            1, -1, 1,       // 1
+            1, 1, 1,        // 2
+            -1, 1, 1,       // 3
 
-            ...[-1, -1, -1], //leftBottomBack
-            ...[1, -1, -1], //rightBottomBack
-            ...[1, 1, -1], //rightTopBack
-            ...[-1, 1, -1], //leftTopBack
-        ].map((v, i) => {
-            const pos = i % 3;
+            -1, -1, -1,     // 4
+            1, -1, -1,      // 5
+            1, 1, -1,       // 6
+            -1, 1, -1,      // 7
 
-            // if(pos == 2) return v + offset.x; // x
-            // if(pos == 1) return v + offset.y; // y
-            if (pos == 2) return v + (append.z ? 2 : 0); // z
-            return v;
-        });
+            -1, -1, -3,     // 8
+            1, -1, -3,      // 9
+            1, 1, -3,       // 10
+            -1, 1, -3,      // 11
+        ];
 
         // prettier-ignore
         const indices = [
-            ...(holes.front ? [] : [0, 1, 2, 0, 2, 3]), // front
-            0, 4, 1, 5, 1, 4, // bottom
-            6, 3, 2, 6, 7, 3, // top
-            ...(holes.back ? [] : [6, 5, 4, 6, 4, 7]), // back
-            0, 3, 4, 3, 7, 4, // left
-            1, 5, 2, 5, 6, 2, // right
+            0, 1, 2, 0, 2, 3,   // front
+
+            0, 4, 1, 5, 1, 4,   // bottom
+            6, 3, 2, 6, 7, 3,   // top
+            0, 3, 4, 3, 7, 4,   // left
+            1, 5, 2, 5, 6, 2,   // right
+
+            4, 8, 5, 9, 5, 8,   // bottom
+            10, 7, 6, 10, 11, 7,   // top
+            4, 7, 8, 7, 11, 8,   // left
+            5, 9, 6, 9, 10, 6,   // right
+
+            10, 9, 8, 11, 10, 8 // back
         ];
 
-        return { vertices, indices };
+        return {
+            vertices,
+            indices,
+        };
     }
 
     view() {
-        const voxels = [
-            this._cube({ holes: {front: true} }),
-            this._cube({ holes: {back: true}, append: { z: true } }),
-        ];
+        const voxels = this._construct();
 
         const geometry = new BufferGeometry();
         geometry.setAttribute(
             'position',
-            new BufferAttribute(
-                new Float32Array(voxels.flatMap((v) => v.vertices)),
-                3
-            )
+            new BufferAttribute(new Float32Array(voxels.vertices), 3)
         );
 
-        const mergedIndices = voxels.reduce(
-            (acc, curr) => ({
-                offset: acc.offset + curr.vertices.length / 3,
-                indices: [
-                    ...acc.indices,
-                    ...curr.indices.map((index) => index + acc.offset),
-                ],
-            }),
-            {
-                indices: [],
-                offset: 0,
-            }
-        );
-        geometry.setIndex(mergedIndices.indices);
+        geometry.setIndex(voxels.indices);
 
         const mesh = new Mesh(
             geometry,
-            new MeshPhongMaterial({ color: 0xaaaaff, flatShading: true, wireframe: true })
+            new MeshPhongMaterial({
+                color: 0xaaaaff,
+                flatShading: true,
+                wireframe: true,
+            })
         );
         mesh.name = 'terrain';
         mesh.position.set(0, 50, 0);
